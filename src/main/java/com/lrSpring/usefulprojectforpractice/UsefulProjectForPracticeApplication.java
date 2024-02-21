9136952ada15e31c4ee9a4b8b0681e8cce4c5161
edelBrock18t <willamedelbrock@gmail.com>
2024-02-22 00:29:31 +0100

    first commit

---
+++ src/main/java/com/lrSpring/usefulprojectforpractice/UsefulProjectForPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.lrSpring.usefulprojectforpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsefulProjectForPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsefulProjectForPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lrSpring/usefulprojectforpractice/UsefulProjectForPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrSpring.usefulprojectforpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsefulProjectForPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
